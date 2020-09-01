c546f817a309a3fd99a2359246c6d8315204b5bc
ehallenborg <46390265+ehallenborg@users.noreply.github.com>
2020-09-01 10:42:34 -0400

    daily solution

--- katas/make-readable.py
+++ katas/make-readable.py
@@ -4,3 +4,8 @@ def make_readable(seconds):
     seconds = seconds - (hours * 3600) - (minutes * 60)
     
     return '%02d:%02d:%02d' % (hours, minutes, seconds)
+
+def make_readable_divmod(seconds):
+    hours, seconds = divmod(seconds, 60 ** 2)
+    minutes, seconds = divmod(seconds, 60)
+    return '%02d:%02d:%02d' % (hours, minutes, seconds)
